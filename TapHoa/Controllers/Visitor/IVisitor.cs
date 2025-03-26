b4896b0498cb94e39066415bf781edd64087dcd0
Inu <thanhtaminu.com>
2025-03-26 21:32:06 +0700

    26/03/2025 21:32

---
+++ TapHoa/Controllers/Visitor/IVisitor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TapHoa.Models;
+
+namespace TapHoa.Controllers.Visitor
+{
+    interface IVisitor
+    {
+        void Visit(SANPHAM sanpham);
+    }
+}
---
+++ TapHoa/Controllers/Visitor/Visitor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TapHoa.Models;
+
+namespace TapHoa.Controllers.Visitor
+{
+    class Visitor : IVisitor
+    {
+        public void Visit(SANPHAM sanpham)
+        {
+            Console.WriteLine("Visitor visited " + sanpham.GetType().Name);
+        }
+    }
+}
