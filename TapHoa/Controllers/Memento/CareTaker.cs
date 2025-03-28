a057fcdf89b2eb673b220b829568226a67e6e8aa
Inu <thanhtaminu.com>
2025-03-29 00:29:54 +0700

    29/03/2025 00:29

---
+++ TapHoa/Controllers/Memento/CareTaker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TapHoa.Controllers.Memento
+{
+    internal class CareTaker
+    {
+    }
+}
